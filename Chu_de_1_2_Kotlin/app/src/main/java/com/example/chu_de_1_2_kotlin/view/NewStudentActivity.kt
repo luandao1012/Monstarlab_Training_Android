381585aa906ae90b8c0ec685065ef2ef6260d91e
Luan <82720293+luandao1012@users.noreply.github.com>
2023-02-12 22:22:03 +0700

    update NewStudentActivity

--- Chu_de_1_2_Kotlin/app/src/main/java/com/example/chu_de_1_2_kotlin/view/NewStudentActivity.kt
+++ Chu_de_1_2_Kotlin/app/src/main/java/com/example/chu_de_1_2_kotlin/view/NewStudentActivity.kt
@@ -3,10 +3,13 @@ package com.example.chu_de_1_2_kotlin.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chu_de_1_2_kotlin.R
+import com.example.chu_de_1_2_kotlin.databinding.ActivityNewStudentBinding
 
 class NewStudentActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewStudentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityNewStudentBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_student)
+        setContentView(binding.root)
     }
 }
