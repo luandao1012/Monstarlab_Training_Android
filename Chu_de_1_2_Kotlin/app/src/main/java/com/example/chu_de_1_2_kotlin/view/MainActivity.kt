ad6b13183f6535f34939587f5eb3684dbc7f2251
Luan <82720293+luandao1012@users.noreply.github.com>
2023-02-12 21:47:21 +0700

    update view MainActivity

--- Chu_de_1_2_Kotlin/app/src/main/java/com/example/chu_de_1_2_kotlin/view/MainActivity.kt
+++ Chu_de_1_2_Kotlin/app/src/main/java/com/example/chu_de_1_2_kotlin/view/MainActivity.kt
@@ -3,15 +3,32 @@ package com.example.chu_de_1_2_kotlin.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chu_de_1_2_kotlin.databinding.ActivityMainBinding
+import com.example.chu_de_1_2_kotlin.model.Student
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listStudents: ArrayList<Student>
+    private lateinit var studentAdapter: StudentAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.btnAdd.setOnClickListener {
+        init()
+        initOnClickListener()
+    }
+
+    private fun init() {
+        listStudents = arrayListOf()
+        studentAdapter = StudentAdapter()
+        studentAdapter.setData(listStudents)
+        binding.rvStudent.layoutManager = LinearLayoutManager(this)
+        binding.rvStudent.adapter = studentAdapter
+    }
+
+    private fun initOnClickListener() {
+        binding.btnActivityAdd.setOnClickListener {
             intent = Intent(this, NewStudentActivity::class.java)
             startActivity(intent)
         }
