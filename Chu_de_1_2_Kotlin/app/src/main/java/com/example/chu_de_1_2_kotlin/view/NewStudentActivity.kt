33885b4ff24c18ff2bbe85047bff1d8ad9955463
Luan <82720293+luandao1012@users.noreply.github.com>
2023-02-12 22:43:59 +0700

    add NewStudentViewModel to NewStudentActivity

--- Chu_de_1_2_Kotlin/app/src/main/java/com/example/chu_de_1_2_kotlin/view/NewStudentActivity.kt
+++ Chu_de_1_2_Kotlin/app/src/main/java/com/example/chu_de_1_2_kotlin/view/NewStudentActivity.kt
@@ -2,14 +2,32 @@ package com.example.chu_de_1_2_kotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.chu_de_1_2_kotlin.R
 import com.example.chu_de_1_2_kotlin.databinding.ActivityNewStudentBinding
+import com.example.chu_de_1_2_kotlin.viewmodel.NewStudentViewModel
 
 class NewStudentActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewStudentBinding
+    private lateinit var viewModel: NewStudentViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityNewStudentBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        init()
+        initOnClickListener()
+    }
+
+    private fun init() {
+        viewModel = ViewModelProvider(this)[NewStudentViewModel::class.java]
+    }
+
+    private fun initOnClickListener() {
+        val name = binding.edtName.text.toString().trim()
+        val yob = binding.edtName.text.toString().trim()
+        val major = binding.edtName.text.toString().trim()
+        val phoneNumber = binding.edtName.text.toString().trim()
+        val type = binding.edtName.text.toString().trim()
+        viewModel.addStudent(name, yob, major, phoneNumber, type)
     }
 }
