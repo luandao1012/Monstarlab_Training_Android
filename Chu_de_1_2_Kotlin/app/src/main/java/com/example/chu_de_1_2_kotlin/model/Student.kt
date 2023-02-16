c4d9afb48c5cdcedfedde043563b99df7d4372c3
Luan <82720293+luandao1012@users.noreply.github.com>
2023-02-16 14:30:54 +0700

    commit

--- Chu_de_1_2_Kotlin/app/src/main/java/com/example/chu_de_1_2_kotlin/model/Student.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chu_de_1_2_kotlin.model
-
-data class Student(
-    var name: String,
-    var yob: String,
-    var major: String,
-    var phoneNumber: String,
-    var type: String
-) {}
