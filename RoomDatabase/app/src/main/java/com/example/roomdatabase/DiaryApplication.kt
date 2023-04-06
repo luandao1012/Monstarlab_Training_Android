e7577d2df8852ea3857624a0c0967e28d72b06c3
Luan <82720293+luandao1012@users.noreply.github.com>
2023-04-06 14:20:45 +0700

    Application

---
+++ RoomDatabase/app/src/main/java/com/example/roomdatabase/DiaryApplication.kt
@@ -0,0 +1,23 @@
+package com.example.roomdatabase
+
+import android.app.Application
+import android.content.Intent
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
+
+class DiaryApplication : Application(), DefaultLifecycleObserver {
+
+    override fun onCreate() {
+        super<Application>.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        super.onStop(owner)
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        startActivity(intent)
+    }
+}
